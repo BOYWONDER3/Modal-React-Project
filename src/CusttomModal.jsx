d45c671f691506a210d54260363f0491ce3c290a
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-08-06 21:37:21 +0100

    second commit

---
+++ src/CusttomModal.jsx
@@ -0,0 +1,10 @@
+export function CusttomModal() {
+    return (
+        <div className="modal-overlay show">
+        <div className="modal">
+          <p>This is a <strong>CUSTOM</strong> modal</p>
+          <button>Close</button>
+        </div>
+      </div>
+    )
+}
